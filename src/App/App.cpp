ae9bb9aefb9d2c1e4aec5eb9af0326b5dff1f7e5
wchui0812 <wchuei0812@gmail.com>
2026-03-13 10:06:32 +0800

    second try

--- src/App/App.cpp
+++ src/App/App.cpp
@@ -11,8 +11,7 @@ void App::Start() {
 }
 
 void App::Update() {
-    
-    //TODO: do your things here and delete this line <3
+
     
     /*
      * Do not touch the code below as they serve the purpose for
